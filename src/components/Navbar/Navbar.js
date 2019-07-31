f946a33029a0cb6a796ca287e1cb9071cd11cb3c
Marcos Orive Izarra <marcos@orive.me>
2019-07-31 20:46:22 +0200

    Completed overlay menu. Now links display correctly. Close button works.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,10 +9,10 @@ export class Navbar extends React.Component{
         this.state = {
             isMenuClosed: true
         }
-        this.displayMenu = this.displayMenu.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
     }
 
-    displayMenu(){
+    toggleMenu(){
         this.setState({
             isMenuClosed : !this.state.isMenuClosed
         })
@@ -23,20 +23,22 @@ export class Navbar extends React.Component{
             <div className="nav-container" style={{backgroundColor: this.props.backgroundColor, color: this.props.fontColor}}>
             <nav>
                 <h3>{this.props.brand}</h3>
-                <span className="hamburguer-icon" onClick={this.displayMenu}>☰</span>
+                <span className="hamburguer-icon" onClick={this.toggleMenu}>☰</span>
                 <div className="navbar-links">
                     {this.props.links.map((node) =>{
                         return(<a href={node.link} style={{color: this.props.fontColor}}>{node.text}</a>);
                     })}
                 </div> 
             </nav>
-            <CSSTransition in={!this.state.isMenuClosed} className="overlay-menu" timeout={800} classNames="slideIn" mountOnEnter unmountOnExit>
+            <CSSTransition in={!this.state.isMenuClosed} className="overlay-container" timeout={800} classNames="slideIn" mountOnEnter unmountOnExit>
                 <div>
-                    <div>Close</div>
-                    <div>
-                        {this.props.links.map((node) =>{
-                            return(<a href={node.link} style={{color: this.props.fontColor}}>{node.text}</a>);
-                        })}
+                    <div className="overlay-buttonClose" onClick={this.toggleMenu}><span>&times;</span></div>
+                    <div className="overlay-menu-wrapper">
+                        <div className="overlay-menu">
+                            {this.props.links.map((node) =>{
+                                return(<a href={node.link} className="overlay-menu-item" style={{color: this.props.fontColor}}>{node.text}</a>);
+                            })}
+                        </div>
                     </div>
                 </div>
             </CSSTransition>
